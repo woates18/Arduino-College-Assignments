5c0719507e08c7967618dc0336c6980ea80a6126
woates18 <133925518+woates18@users.noreply.github.com>
2024-02-21 14:12:34 -0500

    Blink Loop Program pt2
    Modified program to us pointers

---
+++ lab2excercise2.ino
@@ -0,0 +1,52 @@
+int blinkArray[4] = {1,2,3,5};
+
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  // initialize digital pin 8 as an output.
+  pinMode(8, OUTPUT);
+
+  while (!Serial) {
+    ;  // wait for serial port to connect. Needed for native USB port only
+
+  }
+  Serial.println("Press a, b or c");
+}
+
+void loop() {  
+  int *ptr = blinkArray;
+  Serial.println(*ptr);
+  if (Serial.available() > 0) {   // if statment to retrieve user input
+    char thisChar = Serial.read();
+
+    Serial.println(thisChar);
+    switch (thisChar){ // switch statment returning number of blinks to blink loop
+      case 'a':
+        blink(*ptr);
+        break;
+      case 'b':
+        blink(*(ptr + 1));
+        break;
+      case 'c':   
+        blink(*(ptr + 2));
+        break;
+      default:
+        Serial.println("ERROR: neither a, b, or c were pressed");
+        blink(*(ptr + 3));
+        break;
+    }
+  }
+}
+
+void blink(int times) { //blink loop which causes ARDUINO to blink a specified amount of timers
+    for (int i = 0; i < times; i++) {
+      pinMode(8, OUTPUT); 
+      digitalWrite(8, HIGH);  // turn the LED on (HIGH is the voltage level)
+      delay(500);                      // wait for a half second
+      digitalWrite(8, LOW);   // turn the LED off by making the voltage LOW
+      delay(500);                      // wait for a half second
+      Serial.println(times);
+    }
+    Serial.println("Press a, b, or c again");
+}
